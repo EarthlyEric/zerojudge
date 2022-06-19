4933b6e2594b3cacd6fd267e2462554b08ff218e
EarthlyEric <minecaeft.creeper.eric@gmail.com>
2022-06-19 21:16:28 +0800

    ~

--- a004.cpp
+++ a004.cpp
@@ -4,8 +4,8 @@ int main(){
   int y;
   while(cin>>y){
     if(((y%4==0)&&(y%100!=0)) || (y%400==0))
-      cout<<"¶|¦~";
+      cout<<"é–å¹´";
    else
-      cout<<"¥­¦~";
+      cout<<"å¹³å¹´";
   }
 }
--- a005.cpp
+++ a005.cpp
@@ -2,4 +2,12 @@
 using namespace std;
 
 int main(){
+    int i,x[4];
+    cin>>i;
+    for (int r=0;r<+i;r++){
+        for(int s=0;s<4;s++){
+            cin>>x[s];
+            cout<<x[s]<<"";
+        }
+    }
 }
